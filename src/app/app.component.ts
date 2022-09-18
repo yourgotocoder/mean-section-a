b2bbca2ead6596948d87d00af1a96b9e3cbe4327
yourgotocoder <yourgotocoder@gmail.com>
2022-09-18 09:40:32 +0530

    Hints for how to get result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   //    Use click events to get input data
 
   result: number = 110;
   currentInput: string | undefined;
   currentOperation: string | undefined;
 
-  inputTracker: string[] = ["23", "+", "2", "-", "3", "*", "5"];
+  inputTracker: string[] = ['23', '+', '2', '-', '3', '*', '5'];
 
   getInputData = (): string => {
-    return this.inputTracker.join('')
-  }
+    return this.inputTracker.join('');
+  };
 
-  setCurrentInput = (value: string) => {
+  setCurrentInput = (value: string) => {};
 
-  }
-
-  setCurrentOperation = (operation: string) => {
-    
-  }
+  setCurrentOperation = (operation: string) => {};
 
   setResult = (): number => {
-
-    return 0
-  }
-
+    return this.result;
+  };
 }
