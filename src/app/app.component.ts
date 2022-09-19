03719b2f3760322037fc3c42b34907e7902026c7
yourgotocoder <yourgotocoder@gmail.com>
2022-09-19 14:21:52 +0530

    Final hints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,33 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  //    Use click events to get input data
-
   result: number = 110;
   currentInput: string | undefined;
   currentOperation: string | undefined;
 
-  inputTracker: string[] = [
-    '23',
-    '+',
-    '2',
-    '-',
-    '3',
-    '*',
-    '5',
-  ];
+  inputTracker: string[] = ['23', '+', '2', '-', '3', '*', '5'];
 
   getInputData = (): string => {
     return this.inputTracker.join('');
   };
 
+  //    Use click events to get input data
   setCurrentInput = (value: string) => {};
 
   setCurrentOperation = (operation: string) => {};
 
   setResult = (): number => {
     //You can loop over the input tracker array and recompute the result as the values in the array changes
-    //String to number to do mathematical computations
+    //String to number, to do mathematical computations
     //Find a method to check if a string can be converted into a number
     return this.result;
   };
