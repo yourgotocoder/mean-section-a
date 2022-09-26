83d8fc71138aaa43f3769f79eaa5224cac398b8b
yourgotocoder <yourgotocoder@gmail.com>
2022-09-26 15:22:26 +0530

    Further modifications to the ui

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -11,9 +11,30 @@ export class TodoListComponent implements OnInit {
   ngOnInit(): void {}
 
   dummyData: any[] = [
-    { title: 'Breakfast', status: 'done', date: '' },
-    { title: 'Exercise', status: 'pending', date: '' },
-    { title: 'Study', status: 'on-going', date: '' },
-    { title: 'Sleep', status: 'pending', date: ''},
+    {
+      title: 'Breakfast',
+      status: 'done',
+      date: new Date('Mon Sep 26 2022 15:00:00 GMT+0530 (India Standard Time)'),
+    },
+    {
+      title: 'Exercise',
+      status: 'pending',
+      date: new Date('Mon Sep 26 2022 15:00:15 GMT+0530 (India Standard Time)'),
+    },
+    {
+      title: 'Study',
+      status: 'on-going',
+      date: new Date('Mon Sep 26 2022 15:01:08 GMT+0530 (India Standard Time)'),
+    },
+    {
+      title: 'Sleep',
+      status: 'pending',
+      date: new Date('Mon Sep 26 2022 15:01:10 GMT+0530 (India Standard Time)'),
+    },
   ];
+
+  formatDate = (date: Date): string => {
+    const dateToFormat = new Date(date);
+    return `${dateToFormat.getDate()}/${dateToFormat.getMonth() + 1}, ${dateToFormat.getHours()}:${dateToFormat.getMinutes()}:${dateToFormat.getSeconds()}`;
+  }
 }
